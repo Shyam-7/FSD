3addcba6ec2cab75bae4f6ea5536f172bc10875a
Shyam-7 <justsgk07@gmail.com>
2024-08-05 11:48:14 +0530

    pusing all lab files

---
+++ week5/practice.js
@@ -0,0 +1,9 @@
+// Write JS program to compute the sum of array of integers
+const arr = [23, 34, 77, 99, 324];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+
+console.log(sum);
+
